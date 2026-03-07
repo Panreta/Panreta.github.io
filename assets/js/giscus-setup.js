bd0693657b9340096e8815449d77358421fd4652
Panreta <Panreta@users.noreply.github.com>
2026-03-07 09:39:03 +0000

    Deploying to gh-pages from @ Panreta/Panreta.github.io@dd1daeef80a7024341e16ed8663b1a25a6839f16 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Panreta/TingyuZhang.github.io",
+    "data-repo": "Panreta/Panreta.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
