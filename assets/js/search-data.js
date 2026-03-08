f80d5f2324c4e9ccf3b9034c187a953e63ea4dc4
Panreta <120447314+Panreta@users.noreply.github.com>
2026-03-08 22:19:42 +0000

    Deploying to gh-pages from @ Panreta/Panreta.github.io@a1ff5efa90c7c39b29b5b67c603388fa1a252e4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Hi, here is a brief summary of my Resume, you can also click pdf to see.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
